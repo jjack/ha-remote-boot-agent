739196eeea8dbc524e3c0a0a76b6e27dd1c740da
jeremy <jeremy@rocketscientry.com>
2026-04-16 10:45:28 -0500

    test: added get server test

---
+++ cmd/remote-boot-agent/get_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/jjack/remote-boot-agent/internal/config"
+)
+
+func TestGetSelectedOSCommand(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Ubuntu"))
+	}))
+	defer ts.Close()
+
+	cli := &CLI{
+		Config: &config.Config{
+			Host: config.HostConfig{
+				MACAddress: "aa:bb:cc:dd:ee:ff",
+				Hostname:   "test-host",
+			},
+			Bootloader: config.BootloaderConfig{
+				Name: "example",
+			},
+			HomeAssistant: config.HomeAssistantConfig{
+				URL:       ts.URL,
+				WebhookID: "test-webhook",
+			},
+		},
+	}
+
+	cmd := GetSelectedOS(cli)
+
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	err := cmd.Execute()
+
+	w.Close()
+	os.Stdout = oldStdout
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	out, _ := io.ReadAll(r)
+	output := string(out)
+
+	if !strings.Contains(output, "Ubuntu") {
+		t.Errorf("output missing selected OS name Ubuntu: %s", output)
+	}
+}
+
+func TestGetSelectedOSCommand_MissingHAConfig(t *testing.T) {
+	cli := &CLI{
+		Config: &config.Config{
+			Bootloader: config.BootloaderConfig{
+				Name: "example",
+			},
+			HomeAssistant: config.HomeAssistantConfig{
+				URL: "",
+			},
+		},
+	}
+
+	cmd := GetSelectedOS(cli)
+	err := cmd.Execute()
+	if err == nil {
+		t.Fatal("expected error due to missing HA config, got nil")
+	}
+	if !strings.Contains(err.Error(), "homeassistant url not configured") {
+		t.Errorf("unexpected error message: %v", err)
+	}
+}
